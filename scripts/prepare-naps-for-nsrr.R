c1c73eb2070f1795ffdc08bca827ee7552b64420
Runpeng Hu <72607370+runpenghu@users.noreply.github.com>
2025-10-09 14:40:00 -0400

    new file ids to link files; add visit for spout graphics

--- scripts/prepare-naps-for-nsrr.R
+++ scripts/prepare-naps-for-nsrr.R
@@ -6,25 +6,27 @@ library(readxl)
 version <- "0.1.0.pre"
 
 naps_dir <- "/Volumes/bwh-sleepepi-nsrr-staging/20230918-naps/original-data/NAPS1demographicsNSRRtransfer.xlsx"
+naps_dir_1 <- '/Volumes/bwh-sleepepi-nsrr-staging/20230918-naps/original-data/Copy of NAPS1demographicsNSRRtransfer_with_FILE IDS.xlsx'
 
 releasepath <- "/Volumes/bwh-sleepepi-nsrr-staging/20230918-naps/nsrr-prep/_releases/"
 
 ###-------Create the main dataset------
-naps <- read_excel(naps_dir, na = "NA") |>
-  rename_with(tolower)
+naps <- read_excel(naps_dir_1, na = "NA") |>
+  rename_with(tolower) |>
+  rename(
+    file_id = `file id`,
+    study_type = `study type`)|>
+  mutate(visit = 1)
 
 write.csv(naps, file.path(releasepath, paste0(version, "/naps-dataset-", version, ".csv")), na = "", row.names = F)
 
 ###-------Create NSRR harmonized dataset------
 naps_nsrr <- naps |>
-  select(-`study type`)|>
-  rename(nsrrid = naps_id,
+  select(-study_type)|>
+  rename(nsrrid = file_id,
          nsrr_age = age,
          nsrr_sex = sex) |>
-  mutate(nsrr_visit = 1,
-         nsrr_age_gt89 = case_when(
-           nsrr_age > 89  ~ "yes",
-           nsrr_age <= 89 ~ "no"),
+  mutate(nsrr_visit = visit,
          nsrr_sex = case_match(nsrr_sex,
                                1 ~ "male",
                                2 ~ "female"
